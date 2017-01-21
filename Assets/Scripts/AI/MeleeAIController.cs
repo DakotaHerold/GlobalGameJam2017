03a9ea6edc292df0a2c34e479666db7a1fce83be
DakotaHerold <dakota.herold@yahoo.com>
2017-01-20 23:33:56 -0500

    Updated MeleeAIController

--- Assets/Scripts/AI/MeleeAIController.cs
+++ Assets/Scripts/AI/MeleeAIController.cs
@@ -6,10 +6,13 @@ using UnityEngine.AI;
 public class MeleeAIController : MonoBehaviour {
 
     public float speed = 1.0f;
-    public float angularSpeed = 0.0f; 
+    public float angularSpeed = 0.0f;
+    public float damage = 2.0f;
+    public float damageInterval = 2.0f; 
     public Transform target;
-    
 
+    private bool attacking = false; 
+    private SphereCollider trigger; 
     private NavMeshAgent agent;
     private Vector3 moveDirection = Vector3.zero;
 
@@ -30,4 +33,45 @@ public class MeleeAIController : MonoBehaviour {
     {
         agent.SetDestination(target.position);
     }
+
+    // Trigger functions 
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject collidedObject = other.gameObject;
+
+        PlayerScript playerScript = collidedObject.GetComponent<PlayerScript>();
+        DealDamage(playerScript); 
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        GameObject collidedObject = other.gameObject;
+
+        PlayerScript playerScript = collidedObject.GetComponent<PlayerScript>();
+        attacking = true; 
+        StartCoroutine(DealDamageRoutine(playerScript)); 
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        attacking = false; 
+    }
+
+    void DealDamage(PlayerScript player)
+    {
+        player.health -= damage;
+        Debug.Log(player.health); 
+    }
+
+    IEnumerator DealDamageRoutine(PlayerScript player)
+    {
+        while(attacking)
+        {
+            DealDamage(player);
+            yield return new WaitForSeconds(damageInterval); 
+        }
+    }
+
+
+
 }
