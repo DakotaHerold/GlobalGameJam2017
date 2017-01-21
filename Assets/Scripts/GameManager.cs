afa22eb8dc66dc7154a04997f6ff09d3f33852f1
DakotaHerold <dakota.herold@yahoo.com>
2017-01-21 01:08:42 -0500

    Committing changes to enemy prefabs and starting work on spawn points.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,15 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    GameObject[] players;
+
+    // Attributes
+    public GameObject spawnContainer;
+    public bool spawnEnemies = true;
+    public float spawnInterval = 3.0f; 
+
+    private List<Transform> enemySpawnPoints = new List<Transform>(); 
+    private GameObject[] players;
+    private float spawnTimer = 0.0f; 
 	// Use this for initialization
 	void Start () {
         players = GameObject.FindGameObjectsWithTag("player") as GameObject[];
+
+        for(int i = 0; i < spawnContainer.transform.childCount; i++)
+        {
+            enemySpawnPoints.Add(spawnContainer.transform.GetChild(i).GetComponent<Transform>()); 
+        }
+
+        //foreach(Transform t in enemySpawnPoints)
+        //{
+        //    Debug.Log(t.position); 
+        //}
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        spawnTimer += Time.deltaTime; 
+
+        if(spawnTimer > spawnInterval && spawnEnemies)
+        {
+            spawnTimer = 0.0f; 
+        }
 	}
+
+    public void SpawnEnemy()
+    {
+
+    }
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+        while (spawnEnemies)
+        {
+            SpawnEnemy(); 
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
 
 }
