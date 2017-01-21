ba81e44327f7b9bc209e6406ce28f34fba5cda6e
DakotaHerold <dakota.herold@yahoo.com>
2017-01-21 02:02:01 -0500

    Working on enemy spawns and made projectiles destory on impact.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,15 @@ public class GameManager : MonoBehaviour {
 
     // Attributes
     public GameObject spawnContainer;
-    public bool spawnEnemies = true;
-    public float spawnInterval = 3.0f; 
+    public GameObject[] enemies; 
+    public bool shouldSpawn = true;
+    public float spawnInterval;
+    public int numEnemies;
 
-    private List<Transform> enemySpawnPoints = new List<Transform>(); 
+    private List<Transform> enemySpawnPoints = new List<Transform>();
+    private List<Transform> spawnedEnemies = new List<Transform>(); 
     private GameObject[] players;
+    private PlayerScript stickPlayer; 
     private float spawnTimer = 0.0f; 
 	// Use this for initialization
 	void Start () {
@@ -32,24 +36,42 @@ public class GameManager : MonoBehaviour {
 	void Update () {
         spawnTimer += Time.deltaTime; 
 
-        if(spawnTimer > spawnInterval && spawnEnemies)
+        if(spawnTimer > spawnInterval && shouldSpawn)
         {
+            // instantiate enemies here 
+            SpawnEnemies();
+            Debug.Log("Spawning");
             spawnTimer = 0.0f; 
         }
 	}
 
-    public void SpawnEnemy()
-    {
 
-    }
 
-    IEnumerator SpawnEnemyRoutine()
+    public void SpawnEnemies()
     {
-        while (spawnEnemies)
+        int spawnPointIndex = Random.Range(0, enemySpawnPoints.Count); 
+
+        for(int i = 0; i < numEnemies; i++)
         {
-            SpawnEnemy(); 
-            yield return new WaitForSeconds(spawnInterval);
+            int enemyTypeIndex = Random.Range(0, enemies.Length);
+            enemies[enemyTypeIndex].transform.position = enemySpawnPoints[spawnPointIndex].transform.position;
+
+
+            if(enemies[enemyTypeIndex].GetComponent<MeleeAIController>() != null)
+            {
+                MeleeAIController meleeEnemy = enemies[enemyTypeIndex].GetComponent<MeleeAIController>();
+                meleeEnemy.target = players[0].transform; 
+            } else if (enemies[enemyTypeIndex].GetComponent<ShooterAIController>() != null)
+            {
+                ShooterAIController shooterEnemey = enemies[enemyTypeIndex].GetComponent<ShooterAIController>();
+                shooterEnemey.targetObject = players[0];
+            }
+
+            Instantiate(enemies[enemyTypeIndex]); 
         }
     }
+
+
+   
 
 }
---
+++ Assets/Scripts/Projectiles/ProjectileScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+    public bool isAlive = true;
+
+    private Rigidbody body; 
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isAlive)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isAlive = false; 
+    }
+
+
+}
