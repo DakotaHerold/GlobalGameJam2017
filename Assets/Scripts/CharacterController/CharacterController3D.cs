f7020a45ec6ec18faef572544a18b004dfc2709f
Dakota Herold <dakota.herold@yahoo.com>
2017-01-20 20:03:55 -0500

    Added basic character controller.

--- Assets/Scripts/CharacterController/CharacterController3D.cs
+++ Assets/Scripts/CharacterController/CharacterController3D.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class CharacterController3D : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float speed = 1.0f;
+    public float gravity = 9.8f; 
+
+    Rigidbody rigidBody;
+    CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = speed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
 }
